3eae828863752da9a9f2fada62b0d08b53d27cff
Ximena Gamero Goyzueta <ximenagg2007@gmail.com>
2026-09-17 18:25:13 -0500

    Add files via upload

---
+++ ingesta.py
@@ -0,0 +1,10 @@
+import boto3
+
+ficheroUpload = "data.csv"
+nombreBucket = "gcr-output-01"
+
+s3 = boto3.client('s3')
+response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+print(response)
+
+print("Ingesta completada")
